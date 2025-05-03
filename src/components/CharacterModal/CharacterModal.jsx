6b2f0dfd03be12058de6be761fd2e1fba217347c
frnado <fernandogutilla@hotmail.com>
2025-05-03 19:42:53 -0300

    atualizando o modal

--- src/components/CharacterModal/CharacterModal.jsx
+++ src/components/CharacterModal/CharacterModal.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 import "./CharacterModal.css";
 
+import {
+    HeartPulse,
+    User,
+    Dna,
+    Globe,
+    MapPin
+} from "lucide-react";
+
 function CharacterModal({ character, onClose }) {
-    if (!character) return null; 
+    if (!character) return null;
 
     return (
-        <div className="modal show" onClick={onClose}> 
+        <div className="modal show" onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                <h2>{character.name}</h2>
-                <img src={character.image} alt={character.name} />
+                
+                <div className="character-header">
+                    <img src={character.image} alt={character.name} />
+                    <h2>{character.name}</h2>
+
+                </div>
+
+                <div className="character-info">
+                    <p><HeartPulse size={16} /> <strong>Status:</strong> {character.status}</p>
+                    <p><Dna size={16} /> <strong>Species:</strong> {character.species}</p>
+                    <p><User size={16} /> <strong>Gender:</strong> {character.gender}</p>
+                    <p><Globe size={16} /> <strong>Origin:</strong> {character.origin.name}</p>
+                    <p><MapPin size={16} /> <strong>Location:</strong> {character.location.name}</p>
+                </div>
             </div>
         </div>
     );
